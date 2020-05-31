09afe0ab618643b7e81acd528a8f0495d0fde295
Weihao Zhao <48618408+zwhbio2017@users.noreply.github.com>
2020-05-31 17:22:06 +0800

    Create Network_k_mean.py

---
+++ Network_k_mean.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import sys
+
+node_number = int(sys.argv[1])
+network_type = sys.argv[2]
+if network_type == 'SimpleNetwork':
+    network_parameter = sys.argv[3]
+
+if network_type == 'SimpleNetwork':
+    result = pd.read_table('File/' + network_type + '_connection_result_' + str(node_number) + '_' + network_parameter + '.txt', header=None)
+else:
+    result = pd.read_table('File/' + network_type + '_connection_result_' + str(node_number) + '.txt', header=None)
+result.columns = ['connection_num'] + ['gene_num_' + str(i + 1) for i in range(100)]
+k_total = []
+
+for i in range(100):
+    k_total.append(sum(result['connection_num'] * result['gene_num_' + str(i + 1)]) / 1000)
+
+print('mean of K: ' + sum(k_total) / 100)
